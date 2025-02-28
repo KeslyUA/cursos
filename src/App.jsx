d2035e9fe031974914d10d27e3ae0182809d6ce3
KeslyUA <kesly2805@gmail.com>
2025-02-28 15:09:07 -0500

    nada

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 import Principal from'./pages/principal/Principal'
+import Navegacion from'./pages/navegacion/Navegacion'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Principal />
+    <Navegacion />
   )
 }
 
