a8e0f09690220109a262a91fb770f2c1a46bf5ba
KeslyUA <kesly2805@gmail.com>
2025-03-12 20:37:08 -0500

    real3

--- src/pages/explorar/Explorar.jsx
+++ src/pages/explorar/Explorar.jsx
@@ -1,14 +1,28 @@
 import * as React from 'react';
-import '../explorar/explorar.css'
+import '../explorar/explorar.css';
+import { useEffect, useState } from "react";
 
 const Explorar = () => {
+    
+        const [cursos, setCursos] = useState([]);
+        const [cursosSeleccionados, setCursosSeleccionados] = useState([]);
+
+        useEffect(() => {
+            fetch("http://localhost:3001/cursos")
+              .then((response) => response.json())
+              .then((data) => {
+                  console.log("Datos recibidos:", data); 
+                  setCursos(Array.isArray(data) ? data : []); 
+              })
+              .catch((error) => console.error("Error al obtener cursos:", error));
+        }, []);
     return (
          
             <div>
             <style>
             @import url('https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400..700;1,400..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Public+Sans:ital,wght@0,100..900;1,100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
             </style>
-            <div className='fondo'>
+            <div className='fondo-explorar'>
                 <div className='contenedor-uno'>
                 <div className='titulo'>
                     <h1>Escuela de capacitacion empresarial </h1>
@@ -22,26 +36,18 @@ const Explorar = () => {
                 <h2 className='subtitulo'>cursos disponibles:</h2>
                 <br />
                 <div className='combo'>
-                    <div className='box'>
-                        <p>Seguridad Industrial</p>
-                        <div className='img'></div>
-                    </div>
-                    <div className='box'>
-                        <p>Seguridad Industrial</p>
-                        <div className='img'></div>
-                    </div>
-                    <div className='box'>
-                        <p>Seguridad Industrial</p>
-                        <div className='img'></div>
-                    </div>
-                    <div className='box'>
-                        <p>Seguridad Industrial</p>
-                        <div className='img'></div>
-                    </div>
-                    <div className='box'>
-                        <p>Seguridad Industrial</p>
-                        <div className='img'></div>
-                    </div>
+                {cursos.map((curso) =>(console.log("hola",curso.id),
+                       <div key={curso.id} className='box'>
+                            <div className='box-sub'>
+                                <div className='titulo-box'>{curso.titulo}</div>
+                                
+                            </div> 
+                            <div className='img-curso'></div>
+                        </div>
+
+                    )
+                    )}
+                    
                 </div>
                 
                 </div>
