3735cdb12ab6e3f4d20dfab80d308623952548c8
KeslyUA <kesly2805@gmail.com>
2025-03-13 13:39:59 -0500

    real6

--- src/pages/principal/Principal.jsx
+++ src/pages/principal/Principal.jsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import Navegacion from '../navegacion/Navegacion';
-import { Route, Routes } from 'react-router';
+import { Route, Routes,Navigate } from 'react-router';
 import Comunidad from '../comunidad/Comunidad';
 import Novedades from '../novedades/Novedades';
 import Explorar from '../explorar/Explorar';
 import Cursos from '../cursos/Cursos';
 import Publicacion from '../publicacion/Publicacion';
 /* import RutaProtegida from './RutaProgida'; */
+const RutaProtegida = ({ children, rolesPermitidos }) => {
+  const token = localStorage.getItem("token");
+  const cargo = localStorage.getItem("cargo");
+
+  if (!token || !rolesPermitidos.includes(cargo)) {
+    return <Navigate to="/Explorar" />;
+  }
+
+  return children;
+};
 
 const saludo =()=>{
   return(
@@ -19,21 +29,21 @@ const saludo =()=>{
       <Route path="/Explorar" element={<Explorar />}></Route>
       <Route path="/Comunidad" element={<Comunidad />}></Route>
       <Route path="/Novedades" element={<Novedades />}></Route>
-      <Route path='/Cursos' element={<Cursos />}></Route>
-      <Route path='/Publicacion' element={<Publicacion />}></Route>
+      {/* <Route path='/Cursos' element={<Cursos />}></Route>
+      <Route path='/Publicacion' element={<Publicacion />}></Route> */}
      {/*  rutas de administrador */}
-      {/* <Route
+       <Route
           path="/Cursos"
           element={
-            <RutaProtegida rolesPermitidos={["admin"]}>
+            <RutaProtegida rolesPermitidos={["administrador"]}>
               <Cursos />
             </RutaProtegida>
           }
         />
-      <Route path="/Publicacion" element={<RutaProtegida rolesPermitidos={["admin"]}>
+      <Route path="/Publicacion" element={<RutaProtegida rolesPermitidos={["administrador"]}>
               <Publicacion />
             </RutaProtegida>}> 
-      </Route> */}
+      </Route> 
       
     </Routes>
    
