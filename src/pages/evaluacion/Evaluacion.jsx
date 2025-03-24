f40b9c1a2f38ef9f5a3c75274a7746ad88f54571
KeslyUA <kesly2805@gmail.com>
2025-03-24 15:03:43 -0500

    avn-hoy

--- src/pages/evaluacion/Evaluacion.jsx
+++ src/pages/evaluacion/Evaluacion.jsx
@@ -2,7 +2,19 @@ import '../evaluacion/evaluacion.css'
 import React from 'react';
 const Evaluacion = () =>{
     return(
+        
         <div className='fondo-evaluacion'>
+        Evaluacion
+            
+            <div className='cont-supeva'>
+                <div className='cont-evaluacion'>
+               
+            </div>
+            <div className='cont-evaluacion'>
+
+            </div>
+            </div>
+            
         </div>
     )
         
